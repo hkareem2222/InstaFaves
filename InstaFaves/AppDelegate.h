b8acff7331ead13dc9b45f888e6837363f300e90
Husein Kareem <huseinkareem@gmail.com>
2015-05-31 15:13:44 -0500

    Initial Commit

---
+++ InstaFaves/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InstaFaves
+//
+//  Created by Husein Kareem on 5/31/15.
+//  Copyright (c) 2015 Husein Kareem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
